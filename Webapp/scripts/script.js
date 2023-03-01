a68e9ae3bdbf094434566afb47dc3f8ec014882c
dukken06 <kenxavi.8@gmail.com>
2023-03-01 18:07:13 +0900

    あとtwitterのシェアの修正とローディング画面とレスポンシブ

--- Webapp/scripts/script.js
+++ Webapp/scripts/script.js
@@ -18,6 +18,13 @@ calendarShow.addEventListener('click', () => {
     container.classList.toggle('openCalendar');
 })
 
+//ローディング画面を表示する
+// const loadingShow = document.querySelector('.modal__record__button')
+
+// loadingShow.addEventListener('click', () => {
+//     container.classList.toggle('openModal');
+//     container.classList.toggle('openLoading');
+// })
 
 
 //カレンダーの中身
@@ -106,15 +113,230 @@ monthShow.innerHTML = thisMonth
 
 
 const allDays = document.querySelectorAll('.days');
-const thisDay = document.getElementById('today')
+const thisDay = document.getElementById('today');
 
 allDays.forEach(day => {
-    day.addEventListener('click', dayClicked => {
-        thisDay.classList.remove('date-of-today');
-        dayClicked.classList.add('is-chosen');
-        // allDays.classList.remove('date-of-today');
-        // if((allDays.classList.contains('is-chosen') == true) && (allDays != dayClicked)) {
-        //     allDays.classList.remove('is-chosen');
-        // }
+    day.addEventListener('click', () => {
+        //クリックされたら、青いマークがついている今日の日付から、クラス名date-of-todayを削除
+        if(thisDay.classList.contains('date-of-today')) {
+            thisDay.classList.remove('date-of-today');
+        }
+        //クラス名is-chosenが含まれている日付があれば、is-chosenを削除、
+        allDays.forEach(aDay => {
+            if(aDay.classList.contains('is-chosen')) {
+                aDay.classList.remove('is-chosen');
+                let dateNumber = Number(aDay.innerText);
+                // console.log(dateNumber);
+                if(dateNumber < today) {
+                    aDay.classList.add('days-until-today');
+                }
+            }
+        })
+        
+        if(day.classList.contains('days-until-today')) {
+            day.classList.remove('days-until-today');
+        }
+        day.classList.add('is-chosen');
+        let dateEntered = day.innerText
+        const dateForText = year + '年' + month + '月' + dateEntered + '日'
+        TextBoxToday.innerHTML = dateForText
+    })
+})
+
+// ApexCharts.jsを用いて、学習時間の棒グラフを表示
+// グラフを表示
+var options = {
+        series: [{
+            name: 'Time',
+            data: [3,4,5,3,0,0,4,2,2,8,8,2,2,1,7,4,4,3,3,3,2,2,6,2,2,1,1,1,7,8]
+        }],
+    chart: {
+        type: 'bar',
+        height: 350,
+        // ハンバーガーメニューを非表示
+        toolbar: {
+            show: false
+        }
+        // ハンバーガーメニューを非表示(ここまで)
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+    },
+    xaxis: {
+        categories: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+    },
+    fill: {
+        opacity: 1
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + "h"
+            }
+        }
+    }
+};
+
+var chart = new ApexCharts(document.querySelector(".container__hours__graph"), options);
+chart.render();
+
+// 円グラフ(学習言語)
+var options = {
+    series: [30, 20, 10, 5, 5, 20, 20, 10],
+    labels: ['HTML', 'CSS', 'JavaScript', 'PHP', 'Laravel', 'SQL', 'SHELL', '情報システム基礎(その他)'],
+    colors: ['#0345EC','#2272BE','#4EBEDF','#54CEF6','#B29EF3','#6D46EC','#5045F0','#3635C0'],
+    chart: {
+        type: 'donut',
+    },
+    stroke: {
+        width: 0
+    },
+    dataLabels: {
+        style: {
+            fontSize: '6px',
+            fontWeight: '300'
+        },
+        dropShadow: {
+            enabled: false
+        }
+    },
+    plotOptions: {
+        pie: {
+            customScale: 2.4,
+            offsetY: 100,
+            donut: {
+                size: '50%'
+            },
+            dataLabels: {
+                minAngleToShowLabel: 20
+            }
+        }
+    },
+    legend: {
+        show: true,
+        position: 'bottom',
+        offsetY: 100,
+        fontSize: '14px',
+        horizontalAlign: 'left'
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + "h"
+            }
+        }
+    }
+};
+
+var chart = new ApexCharts(document.querySelector(".container__content__language__chart"), options);
+chart.render();
+
+// 円グラフ(学習コンテンツ)
+var options = {
+    series: [40, 20, 40],
+    labels: ['N予備校', 'ドットインストール', '課題'],
+    colors: ['#0345EC','#2272BE','#4EBEDF'],
+    chart: {
+        type: 'donut',
+    },
+    stroke: {
+        width: 0
+    },
+    dataLabels: {
+        style: {
+            fontSize: '6px',
+            fontWeight: '300'
+        },
+        dropShadow: {
+            enabled: false
+        }
+    },
+    plotOptions: {
+        pie: {
+            customScale: 1.9,
+            offsetY: 90,
+            donut: {
+                size: '50%'
+            },
+            dataLabels: {
+                minAngleToShowLabel: 20
+            }
+        }
+    },
+    legend: {
+        show: true,
+        position: 'bottom',
+        offsetY: 100,
+        fontSize: '14px',
+        horizontalAlign: 'left'
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + "h"
+            }
+        }
+    }
+};
+
+var chart = new ApexCharts(document.querySelector(".container__content__material__chart"), options);
+chart.render();
+
+// 記録・投稿ボタンを取得
+const postButton = document.querySelector('.modal__record__button')
+
+// .c-box-itemをclickしたら、.c-box-itemのcheckboxとbackgroundのcolorを変える
+const contentsItems = document.querySelectorAll('.c-box-item');
+const contentsItemsCheckbox = document.querySelectorAll('.c-box-item i');
+
+contentsItems.forEach(contentsItem => {
+    contentsItem.addEventListener('click', () => {
+        contentsItem.classList.toggle('checked');
+    })
+})
+// .l-box-itemをclickしたら、.l-box-itemのcheckboxとbackgroundのcolorを変える
+const languageItems = document.querySelectorAll('.l-box-item');
+const languageItemsCheckbox = document.querySelectorAll('.l-box-item i');
+
+languageItems.forEach(languageItem => {
+    languageItem.addEventListener('click', () => {
+        languageItem.classList.toggle('checked');
+    })
+})
+
+// .share__buttonをclickして、記録・投稿ボタンを押したら、twitter画面を表示し、テキスト内容を反映する関数を作成
+const tweetText = () => {
+    const commentForTwitter = document.getElementById('comment__for__twitter');
+    const textContent = commentForTwitter.value;
+    let url = document.location.href;
+    
+    url = "http://twitter.com/share?url=" + "&text=" + textContent;
+    window.open(url,"_blank");
+    
+}
+
+
+// .share__buttonをclickしたら、.share__button iのcolorが変わる
+const shareButton = document.querySelector('.share__button');
+const shareButtonCheckbox = document.querySelector('.share__button i');
+
+shareButton.addEventListener('click', () => {
+    shareButtonCheckbox.classList.toggle('checked');
+    postButton.addEventListener('click', () => {
+        // console.log(commentForTwitter.value);
+        // window.location.href = 'https://twitter.com/compose/tweet'; 
+        tweetText();
     })
 })
